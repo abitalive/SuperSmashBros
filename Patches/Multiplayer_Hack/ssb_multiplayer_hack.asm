7468c3d4fb438e5def78066cfec4f042d4f70fbf
abitalive <abitalive@users.noreply.github.com>
2016-07-05 02:18:18 +1000

    Improve timed stock scoring

--- Patches/Multiplayer_Hack/ssb_multiplayer_hack.asm
+++ Patches/Multiplayer_Hack/ssb_multiplayer_hack.asm
@@ -163,9 +163,14 @@ base 0x8013A778
 ori t6, r0, 0x01
 bnel t5, t6, 0x8013A790 // Show stock icons
 
+origin 0x155DA8
+base 0x80136C08
+j TimeScoring // Show correct places
+nop
+
 origin 0x156564
 base 0x801373C4
-j TimedStockScore // Show correct places
+j TimedStock // Show correct places
 
 // Allow start immediately on versus CSS
 origin 0x138CF0
@@ -594,21 +599,46 @@ scope ExtraCostumes: {
 }
 
 // Timed stock
-scope TimedStockScore: {
+scope TimeScoring: {
+  lui t6, 0x800A
+  lb t6, 0x4D0B (t6)
+  ori t7, 0x01
+  beq t6, t7, Time // If mode == stock
+  Stock:
+    ori t6, r0, 0x74
+    multu a0, t6
+    mflo t6
+    lui t7, 0x800A
+    addu t7, t6
+    lb v0, 0x4D33 (t7) // Points = stocks
+    addiu v0, 0x01
+    b End
+    nop
+  Time: // Else
+    sll v1, a0, 0x02
+    lui t6, 0x8014
+    lui t7, 0x8014
+    addu t7, v1
+    addu t6, v1
+    lw t6, 0x9B80 (t6) // Load KOs
+    lw t7, 0x9B90 (t7) // Load deaths
+    subu v0, t6, t7 // Points = KOs - deaths
+  End:
+    jr ra
+    nop
+}
+
+scope TimedStock: {
   ori t0, r0, 0x01
-  beq t6, t0, Time
+  beq t6, t0, Time // If mode == time use time scoring
   nop
   lui t0, 0x800A
   lw t0, 0x4D1C (t0)
-  beq t0, r0, TimeNoKo
+  beq t0, r0, Time // Else if timer == 0 use time scoring
   nop
   Stock:
-    j 0x801373CC
+    j 0x801373CC // Else use stock scoring
     nop
-  TimeNoKo: // If mode == stock and timer == 0
-    lui t0, 0xA013
-    ori t1, r0, 0x7025 // or t6, r0, r0
-    sw t1, 0x6C1C (t0) // Don't load KOs
   Time:
     j 0x801373F4
     nop
