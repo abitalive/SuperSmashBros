23eb57157f24649e13d30d1b890ac8b7fea4f5b7
abitalive <abitalive@users.noreply.github.com>
2016-08-21 21:39:00 +1000

    Optimize FinalDestination function

--- Patches/Multiplayer_Hack/ASM/final_destination.asm
+++ Patches/Multiplayer_Hack/ASM/final_destination.asm
@@ -9,20 +9,17 @@ pullvar pc, origin
 scope FinalDestination: {
   lbu v0, 0x01 (t7) // Original instruction
   lui t0, 0x800A
+  lbu t1, Stage (t0) // Stage
+  lli t2, 0x10
+  bne t1, t2, End // If stage == Final Destination
+  nop
   lbu t1, ScreenCurrent (t0) // Mode
   lli t2, 0x16
-  beq t1, t2, TrainingVersus // If mode == versus
+  beql t1, t2, End // And mode == versus; return Battlefield index
+  lli v0, 0x0E
   lli t2, 0x36
-  beq t1, t2, TrainingVersus // Or mode == training
-  nop
-  b End
-  nop
-  TrainingVersus:
-    lbu t1, Stage (t0) // Stage
-    lli t2, 0x10
-    bne t1, t2, End // If stage == Final Destination
-    nop
-    lli v0, 0x0E // Return Battlefield index
+  beql t1, t2, End // Or mode == training; return Battlefield index
+  lli v0, 0x0E
   End:
    jr ra
    nop
