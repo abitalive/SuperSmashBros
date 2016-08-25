23bae19880596dcacc00289031d2a53ac0da5592
abitalive <abitalive@users.noreply.github.com>
2016-08-25 10:49:38 +1000

    Add bugfixes

---
+++ Patches/Multiplayer_Hack/ASM/random_stage_fix.asm
@@ -0,0 +1,5 @@
+// Random stage fix
+
+origin 0x042CDF
+base 0x800A3F8C
+db 0xFF
---
+++ Patches/Multiplayer_Hack/ASM/volume_fix.asm
@@ -0,0 +1,11 @@
+// Volume fixes
+
+// Boot volume
+origin 0x02D004
+base 0x8002C404
+lli t9, 0x7800
+
+// Sound test volume
+origin 0x1884D0
+base 0x80132100
+lli a1, 0x7800
